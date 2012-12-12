fa6745bd88fd88f1e3f9c5757e99cc7b61e4bb14
Riccardo Degan <riccardo.degan@diginess.it>
2012-12-12 14:45:18 +0100

    Fix calculation of the discount

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -2,8 +2,8 @@ module Spree
   module ProductsHelper
     def product_price(product)
       total = product.price
-      product.possible_promotions.each do |promotion|
-        promotion.promotion_actions.each do |action|
+      product.promotion_rules.each do |promotion_rule|
+        promotion_rule.promotion.promotion_actions.each do |action|
           total += action.compute_amount(product)
         end
       end
